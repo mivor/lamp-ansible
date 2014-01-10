fcfac49ca692b3c8a997c4f8d9b268a1139ea925
Ferenc Grecu <mivor20@gmail.com>
2014-01-10 18:36:51 +0200

    Add script for running ansible

--- ansible.sh
+++ ansible.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+vssh() {
+    identity=$(vagrant ssh-config | grep IdentityFile  | awk '{print $2}')
+    port=$(vagrant ssh-config | grep Port | awk '{print $2}')
+    ssh -t vagrant@127.0.0.1 -p $port -i $identity $@
+}
+
+vans() {
+    local prov_dir='/vagrant/provision'
+    vssh ansible-playbook -i $prov_dir/inventory/$1 $prov_dir/$2
+}
+
+vans $@
